fc0b686816509ebeb1599fa226d4c16938bdc25a
Saad Ahmed <dropheaven@live.com>
2019-11-24 16:18:12 -0500

    complete mini pokedex project

---
+++ pokedex/src/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Pokedex from "./Pokedex";
+import PokeGame from "./PokeGame";
+
+class App extends Component {
+  render() {
+    return <PokeGame />;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
